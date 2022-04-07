d958fc680a2c0d8b91b9b4ff3cfa4cbe699aa581
joemu <joe.mueller@uzh.ch>
2022-04-07 19:24:05 +0200

    -User can see their info in their profile page -User can navigate back to the main menu from their profilePage -api file changed so authorization should work (->David)

--- src/components/views/UserPage.js
+++ src/components/views/UserPage.js
@@ -1,19 +1,16 @@
 import React, {useEffect, useState} from "react";
 import Header from "./Header";
-import {api, handleError} from 'helpers/api';
+import {api} from 'helpers/api';
 import BaseContainer from "../ui/BaseContainer";
 import 'styles/views/UserPage.scss'
-import User from "../../models/User";
 
 
 const PlayerProfile = ({user}) =>(
-
     <div className="userPage container">
         <ul className="userPage player-info-container">Username: {user.username}</ul>
         <ul className="userPage player-info-container">Name: {user.name}</ul>
         <ul className="userPage player-info-container">Gender: {user.gender}</ul>
         <ul className="userPage player-info-container">Birthday: {user.birthday}</ul>
-
     </div>
 )
 
@@ -25,18 +22,33 @@ const UserPage = () =>{
         async function getUser(){
             const id = localStorage.getItem("id")
             const response = await api.get('/users/'+id)
-            const user = new User(response.data);
+            setUser(response.data);
             console.log(response)
         }
         getUser()
 
     }, []);
+    let profile = (
+        <div className="userPage container">
+            <ul className="userPage player-info-container">Username: username</ul>
+            <ul className="userPage player-info-container">Name: name</ul>
+            <ul className="userPage player-info-container">Gender: gender</ul>
+            <ul className="userPage player-info-container">Birthday: birthday</ul>
+        </div>
+    )
+
+    if(user){
+        profile  = (
+            <PlayerProfile user={user}/>
+        )
+    }
+
     return(
         <React.Fragment>
             <Header view="userPage"/>
             <BaseContainer>
                 <div className="userPage main-container">
-                    <PlayerProfile user ={user}/>
+                    {profile}
                 </div>
             </BaseContainer>
         </React.Fragment>
