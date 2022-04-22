52084b38ad216dc862081c62fb4bb0b9abe3be43
anhuwi <andreas.huwiler@uzh.ch>
2022-04-23 01:11:02 +0200

    playWhites

--- src/components/views/PlayWhites.js
+++ src/components/views/PlayWhites.js
@@ -1,15 +1,38 @@
 import React, {useEffect, useState} from 'react';
-import {useHistory} from 'react-router-dom';
+import {useHistory, useLocation} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/WhiteCardSelection.scss";
 import Header from "./Header";
 import CardButton from "../ui/CardButton";
 import MenuContainer from "../ui/MenuContainer";
+import {api, handleError} from "../../helpers/api";
+
+//TODO: fetch a game of a random user (todo) and your white cards (done)
 
 
 const PlayWhites = () => {
   // use react-router-dom's hook to access the history
   const history = useHistory();
+    const [cards, setCards] = useState(null)
+
+
+    // Because of rendering reasons, we use location here, which allows passing states around components.
+    // Here we get the state from Registration / Login, because the localStorage might not have been updated yet.
+    // If token / userId is in localStorage, we use this, else we use the state passed from login / registration
+    // if we're coming from these components, else we use null and trigger a call to the server which we shall then handle.
+    const location = useLocation()
+    let userId = null
+    let token = null
+    try {
+        userId = (localStorage.getItem("id")) ? localStorage.getItem("id") : location.state.id
+    } catch (e) {
+        console.log("No userId found!")
+    }
+    try {
+        token = (localStorage.getItem("token")) ? localStorage.getItem("token") : location.state.token
+    } catch (e) {
+        console.log("No token found!")
+    }
 
   // define a state variable (using the state hook).
   // if this variable changes, the component will re-render, but the variable will
@@ -17,39 +40,54 @@ const PlayWhites = () => {
   // a component can have as many state variables as you like.
   // more information can be found under https://reactjs.org/docs/hooks-state.html
 
+    // fetch the blackCards from the server (it is the server's responsibility to give us 8 cards)
+    useEffect(() => {
+        async function fetchWhiteCards() {
+            try {
+                const response = await api.get(`users/${userId}/games/whiteCards`,
+                    {
+                        // reconfiguration might be necessary in case token is not in localStorage here
+                        headers: {
+                            "authorization": token
+                        }
+                    });
+                setCards(response.data)
+            }
+            catch (error) {
+                console.error("Details:", error);
+                alert("Invalid Input:\n " + handleError(error));
+            }
+        }
+        fetchWhiteCards();
+    }, []);
+
+    // placeholder in case of failure
+    let content = <div>No Content Available</div>
+
+    if(cards) {
+        // TODO: onClick: ask to confirm this card, then make api call to submit this card choice, then reload page
+        content =
+            <ul className={"game card-list"}>
+                {cards.map(card => (
+                    <CardButton className={"card whiteCard"}
+                                children={card.text}
+                                key={card.id}
+                    />
+                ))}
+            </ul>
+    }
+
+
   return (
     <React.Fragment>
-      <Header view="game"/>
-      <h1>Pick a white card</h1>
+        <Header view="game"/>
+        <div className={"game description"}>
+            <h1>Pick a white card</h1>
+        </div>
 
-      <MenuContainer className={"menu container"}>
-
-        {/* some card */}
-        <CardButton
-          className="card whiteCard"
-          // TODO: onClick: ask to confirm this card, then make api call to submit this card choice, then reload page
-          // onClick={() => history.push('/game/playWhites')}
-          children={"This is a white card"}
-        />
-
-        {/* some card */}
-        <CardButton
-          className="card whiteCard"
-          // TODO: onClick: ask to confirm this card, then make api call to submit this card choice, then reload page
-          // onClick={() => history.push('/game/playWhites')}
-          children={"This is a white card"}
-        />
-
-        {/* some card */}
-        <CardButton
-          className="card whiteCard"
-          // TODO: onClick: ask to confirm this card, then make api call to submit this card choice, then reload page
-          // onClick={() => history.push('/game/playWhites')}
-          children={"This is a white card"}
-        />
-
-
-      </MenuContainer>
+        <BaseContainer className={"menu container"}>
+            {content}
+        </BaseContainer>
     </React.Fragment>
   );
 }
