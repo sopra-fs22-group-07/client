8eb1e2bc26c17c2b9111f7053ae3a3ccda9353fd
joemu <joe.mueller@uzh.ch>
2022-04-24 12:36:09 +0200

    Added RateWHites Class

---
+++ src/components/views/RateWhites.js
@@ -0,0 +1,17 @@
+/*
+For Rating the White Cards that got played on your Black Cards
+ */
+
+import Header from "./Header";
+
+const RateWhites = () => {
+
+    return(
+        <React.Fragment>
+            <Header view="game"/>
+            <div> TEXT </div>
+        </React.Fragment>
+    )
+}
+
+export default RateWhites;
