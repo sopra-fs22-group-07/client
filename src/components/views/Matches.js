d1cbcc124f2ffc31f9304ea382dd20bd399345ba
Laurin van den Bergh <laurin.vandenbergh@gmail.com>
2022-04-25 18:15:40 +0200

    fetching and displaying all matched users

--- src/components/views/Matches.js
+++ src/components/views/Matches.js
@@ -5,146 +5,76 @@ import "styles/views/WhiteCardSelection.scss";
 import Header from "./Header";
 import CardButton from "../ui/CardButton";
 import {api, handleError} from "../../helpers/api";
-import PropTypes from "prop-types";
 
-/*
- Viewing the matches
- */
+
+// Viewing the matches
 const Matches = () => {
-//   // use react-router-dom's hook to access the history
-//   const history = useHistory();
-//   const[blackCard, setBlackCard] = useState(null)
-//   const[gameId, setGameId] = useState(null)
-//   const [cards, setCards] = useState(null)
-//     const [count, setCount] = useState(0)
-//   const userId = localStorage.getItem("id")
-//   const token = localStorage.getItem("token")
+    // use react-router-dom's hook to access the history
+    const history = useHistory();
+    const userId = localStorage.getItem("id")
+    const token = localStorage.getItem("token")
+    const [matchedUsers, setMatchedUsers] = useState(null)
 
-//     // function defines what is happening, when a white card gets selected. also renders the white cards
-//     const WhiteCard = ({card}) => {
+    // functions that gets the matches from the server
+    const getMatches = async () => {
+        try {
+            const response = await api.get(`users/${userId}/matches`);
+            console.log(response.data)
+            // convert the response to an array of objects
+            const matchedUsersArray = response.data.map(match => {
+                return {
+                    id: match.id,
+                    name: match.name,
+                    username: match.username,
+                    birthday: match.birthday,
+                    creationDate: match.creationDate,
+                    gender: match.gender,
+                    status: match.status
+                }
+            })
+            setMatchedUsers(matchedUsersArray)
+        } catch (error) {
+            console.error("Details:", error);
+            alert("Invalid Input:\n " + handleError(error));
+        }
+    }
 
-//         // put the white Card and userId to the game on a Server, reloads useEffects
-//         const selectCard = async ()  => {
-//             if (window.confirm("Press OK to confirm this card")){
-//                 let cardId = card.id
-//                 const requestBody = JSON.stringify({gameId});
-//                 // card gets played
-//                 try {
-//                     await api.post(`users/${userId}/whiteCards/${cardId}`, requestBody);
-//                 } catch (error) {
-//                     console.error("Details:", error);
-//                     alert("Invalid Input:\n " + handleError(error));
-//                 }
-//                 // next card gets displayed, use for statistic, reloads useEffects
-//                 setCount(count + 1)
-//             }
-//             else{
-//                 console.log("Card was not played!")
-//             }
-//         }
-//         // design of white card
-//         return(
-//             <CardButton className={"card whiteCard"}
-//                         onClick={() => selectCard()}
-//                         children={card.text}
-//                         key={card.id}
-//             />
-//         );
+    // get all matches on page load
+    useEffect(() => {
+        getMatches()
+    }, [])
 
-//     };
+    return (
+        <React.Fragment>
+            <Header view="game"/>
+            <div className={"game description"}>
+                <h1>Users with whom you matched:</h1>
+            </div>
 
-//     // test if white card is of type card
-//     WhiteCard.propTypes = {
-//         card: PropTypes.object
-//     };
-
-//   // define a state variable (using the state hook).
-//   // if this variable changes, the component will re-render, but the variable will
-//   // keep its value throughout render cycles.
-//   // a component can have as many state variables as you like.
-//   // more information can be found under https://reactjs.org/docs/hooks-state.html
-
-//     useEffect(() => {
-//         // the game of a random user gets fetched
-//         async function fetchGame() {
-//             try {
-//                 const response = await api.get(`users/${userId}/games/blackCards`,
-//                     {
-//                         headers: {
-//                             "authorization": token
-//                         }
-//                     });
-//                 setBlackCard(response.data.blackCard)
-//                 setGameId(response.data.gameId)
-//                 console.log(response.data)
-//             }
-//             catch (error) {
-//                 console.error("Details:", error);
-//                 alert("Invalid Input:\n " + handleError(error));
-//             }
-//         }
-
-//         // the white cards of the user who is playing gets fetched
-//         async function fetchWhiteCards() {
-//             try {
-
-//                 const response = await api.get(`users/${userId}/games/whiteCards`,
-//                     {
-//                         headers: {
-//                             "authorization": token
-//                         }
-//                     });
-//                 setCards(response.data)
-//                 console.log(response.data)
-//             }
-//             catch (error) {
-//                 console.error("Details:", error);
-//                 alert("Invalid Input:\n " + handleError(error));
-//             }
-//         }
-//         fetchGame();
-//         fetchWhiteCards();
-//     }, [count]); // when count gets changed, new call to useEffects
-
-//     // placeholder in case of failure
-//     let cardsContent = <div>No white cards available</div>
-//     let blackCardContent = <div>No black card available</div>
-//     // white cards get displayed if fetched
-//     if(cards) {
-//         cardsContent =
-//             <ul className={"game card-list"}>
-//                 {cards.map(card => (
-//                     <WhiteCard card={card} key={card.id}/>
-//                 ))}
-//             </ul>
-//     }
-//     // black card gets displaced id fetched
-//     if(blackCard){
-//         blackCardContent =
-//             <CardButton className={"card blackCard"}>
-//                 {blackCard.text}
-//             </CardButton>
-//     }
-
-//   return (
-//     <React.Fragment>
-//         <Header view="game"/>
-//         <div className={"game description"}>
-//             <h1>Use a White Card to fill in the Blank</h1>
-//         </div>
-
-//         <BaseContainer className={"menu container"}>
-//             {blackCardContent}
-//         </BaseContainer>
-//         <div className={"game description"}>
-//             <h1>Pick a white card</h1>
-//         </div>
-
-//         <BaseContainer className={"menu container"}>
-//             {cardsContent}
-//         </BaseContainer>
-//     </React.Fragment>
-//   );
+            <BaseContainer className={"menu container"}>
+                {/* display all users in matchedUsers if there are any */}
+                {matchedUsers ? matchedUsers.map(user => {
+                    return (
+                        <CardButton
+                            key={user.id}
+                            className={"card whiteCard"}
+                            // onClick={() => history.push(`/game/${user.id}`)}
+                        >
+                            {`Username: ${user.username}`}
+                            <br/>
+                            {`Name: ${user.name}`}
+                            <br/>
+                            {`Status: ${user.status}`}
+                            <br/>
+                            {`Birthday: ${user.birthday}`}
+                            <br/>
+                            {`Gender: ${user.gender}`}
+                        </CardButton>
+                    )
+                }) : "No Matches Found"}
+            </BaseContainer>
+        </React.Fragment>
+    );
 }
 
 export default Matches;
