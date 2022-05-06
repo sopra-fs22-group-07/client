b2778b45d1d148a80a4c8e43d511d68d1304bd2d
joemu <joe.mueller@uzh.ch>
2022-05-06 17:01:50 +0200

    -Working on User Edit PAge

--- src/components/views/EditUserPreferencePage.js
+++ src/components/views/EditUserPreferencePage.js
@@ -1,15 +1,15 @@
-
-/*
-Edit User Preferences Page
- */
-
-
 import React from "react";
 import Header from "./Header";
 import BaseContainer from "../ui/BaseContainer";
 import {Button} from "../ui/Button";
 import {useHistory} from "react-router-dom";
+import Slider from 'react-rangeslider';
+import 'react-rangeslider/lib/index.css'
 
+
+/*
+Edit User Preferences Page
+ */
 const EditUserPreferencePage = () =>{
     const id = localStorage.getItem("id")
 
@@ -20,6 +20,11 @@ const EditUserPreferencePage = () =>{
     let editPreferences = (
         <div className="login form">
             <h2 className="login title"> Set Preferences </h2>
+            <Slider
+                min={18}
+                max={99}
+                step={1}
+            />
             {/*TODO: Insert Prefences here*/}
             <div className= "userPage fixed-button-container">
                 <div className= "userPage moving-button-container">
