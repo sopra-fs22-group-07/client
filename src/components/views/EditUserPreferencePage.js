76074e1492b967b33b4cad5cd6cf34e04678e21a
joemu <joe.mueller@uzh.ch>
2022-05-06 17:45:55 +0200

    -Tested out the PutPreferences. It works now, by passing an Array to Json which then gets parsed to set in backend

--- src/components/views/EditUserPreferencePage.js
+++ src/components/views/EditUserPreferencePage.js
@@ -5,6 +5,9 @@ import {Button} from "../ui/Button";
 import {useHistory} from "react-router-dom";
 import Slider from 'react-rangeslider';
 import 'react-rangeslider/lib/index.css'
+import {api, handleError} from "../../helpers/api";
+
+
 
 
 /*
@@ -47,7 +50,19 @@ const EditUserPreferencePage = () =>{
         </div>
     )
 
-    function doEditPreferences(){
+    const doEditPreferences = async () =>{
+        try{
+            let minAge = 30
+            let maxAge = 40
+            let genderPreferences = ["OTHER"]
+            const requestBody = JSON.stringify({minAge, maxAge, genderPreferences})
+            await api.put(`/users/${id}/preferences`, requestBody);
+
+            // Editing users preferences successfully worked --> navigate to the route /userprofile in the router
+            history.push(`/users/${id}`);
+        } catch (error) {
+            alert(`Something went wrong while changing the User Preferences: \n${handleError(error)}`);
+        }
     }
 
     function doCancel() {
