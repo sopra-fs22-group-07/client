57dd4556129273221387116a4c654ae5be63f0a4
joemu <joe.mueller@uzh.ch>
2022-05-06 15:20:38 +0200

    -Setting up Edit Preferences Page

--- src/components/views/EditUserPreferencePage.js
+++ src/components/views/EditUserPreferencePage.js
@@ -4,10 +4,62 @@ Edit User Preferences Page
  */
 
 
+import React from "react";
+import Header from "./Header";
+import BaseContainer from "../ui/BaseContainer";
+import {Button} from "../ui/Button";
+import {useHistory} from "react-router-dom";
+
 const EditUserPreferencePage = () =>{
+    const id = localStorage.getItem("id")
+
+    const history = useHistory();
+
+
+
+    let editPreferences = (
+        <div className="login form">
+            <h2 className="login title"> Set Preferences </h2>
+            {/*TODO: Insert Prefences here*/}
+            <div className= "userPage fixed-button-container">
+                <div className= "userPage moving-button-container">
+                    <Button
+                        width="100%"
+                        onClick={() => doEditPreferences()}
+                    >
+                        Save
+                    </Button>
+                </div>
+                <div className= "userPage moving-button-container">
+                    <Button
+                        width="100%"
+                        onClick={() => doCancel()}
+                    >
+                        Cancel
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+
+    function doEditPreferences(){
+    }
+
+    function doCancel() {
+        history.push(`/users/${id}`)
+    }
+
+
     return(
-        <div> edit user preferences </div>
-        )
+        <React.Fragment>
+            <Header view="userPage"/>
+            <BaseContainer>
+                <div className="login container">
+                    {editPreferences}
+                </div>
+            </BaseContainer>
+        </React.Fragment>
+    )
 }
 
 export default EditUserPreferencePage
