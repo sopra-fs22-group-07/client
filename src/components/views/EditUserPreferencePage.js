93415e006260c402b8d4adb31ddfb66841ca621b
joemu <joe.mueller@uzh.ch>
2022-05-12 15:01:10 +0200

    -AgeSlider works now

--- src/components/views/EditUserPreferencePage.js
+++ src/components/views/EditUserPreferencePage.js
@@ -1,11 +1,10 @@
-import React from "react";
-import Header from "./Header";
+import React, {useEffect, useState} from "react";
 import BaseContainer from "../ui/BaseContainer";
 import {Button} from "../ui/Button";
 import {useHistory} from "react-router-dom";
 import {api, handleError} from "../../helpers/api";
-
-
+import 'styles/views/LoginRegistration.scss'
+import Slider from "@mui/material/Slider";
 
 
 /*
@@ -16,12 +15,53 @@ const EditUserPreferencePage = () =>{
 
     const history = useHistory();
 
+    const [agePreference, setAgePreference] = useState([20, 37]) //TODO: set as current age preference
+    const [user, setUser] = useState(null)
+    const [genderPreference, setGenderPreference] = useState(null)
+
+    useEffect(() => {
+        async function getUser(){
+            try{
+                const response = await api.get('/users/'+id)
+                setUser(response.data);
+                setAgePreference([response.data.minAge, response.data.maxAge])
+            } catch (error) {
+                console.error("Details:", error);
+                alert("Invalid Input:\n " + handleError(error));
+            }
+        }
+        getUser()
+    }, []);
+
+
+
+    const handleChange = (event, newValue, activeThumb) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            setAgePreference([Math.min(newValue[0], agePreference[1]), agePreference[1]]);
+        } else {
+            setAgePreference([agePreference[0], Math.max(newValue[1], agePreference[0])]);
+        }
+    };
 
 
     let editPreferences = (
         <div className="login form">
             <h2 className="login title"> Set Preferences </h2>
-            {/*TODO: Insert Prefences here*/}
+            <div className="login container-title">Edit Age Preferences:</div>
+            <Slider
+                value={agePreference}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                min={18}
+                max={99}
+                disableSwap
+            />
+            <div className="login container-title">Edit Gender Preferences:</div>
+
             <div className= "userPage fixed-button-container">
                 <div className= "userPage moving-button-container">
                     <Button
@@ -45,8 +85,8 @@ const EditUserPreferencePage = () =>{
 
     const doEditPreferences = async () =>{
         try{
-            let minAge = 18
-            let maxAge = 2143
+            let minAge = 30
+            let maxAge = 52
             let genderPreferences = ["OTHER"]
             const requestBody = JSON.stringify({minAge, maxAge, genderPreferences})
             await api.put(`/users/${id}/preferences`, requestBody);
@@ -65,7 +105,6 @@ const EditUserPreferencePage = () =>{
 
     return(
         <React.Fragment>
-            <Header view="userPage"/>
             <BaseContainer>
                 <div className="login container">
                     {editPreferences}
--- src/components/views/UserPage.js
+++ src/components/views/UserPage.js
@@ -40,7 +40,6 @@ const UserPage = () =>{
             }
         }
         getUser()
-
     }, []);
 
 
