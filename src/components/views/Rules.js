732719b5ff0b24de589944d80ba110d4723e8f8e
joemu <joe.mueller@uzh.ch>
2022-05-25 14:15:49 +0200

    -Added Links to Text

--- src/components/views/Rules.js
+++ src/components/views/Rules.js
@@ -2,9 +2,62 @@ import React from "react";
 import BaseContainer from "../ui/BaseContainer";
 import 'styles/views/Rules.scss'
 import 'styles/views/LoginRegistration.scss';
+import {useHistory} from "react-router-dom";
+import {handleError} from "../../helpers/api";
 
 
 const Rules = () =>{
+    const history = useHistory();
+    const id = localStorage.getItem("id")
+
+
+    function pushURL(url){
+        try {
+            history.push(url)
+        } catch (error) {
+            alert(`Something went wrong during the logout: \n${handleError(error)}`);
+        }
+    }
+
+    function goToHand() {
+        pushURL(`/game/hand`)
+    }
+
+    function goToPlayWhites() {
+        pushURL(`/game/playWhites`)
+    }
+
+    function goToRateWhites() {
+        pushURL(`/game/rateWhites`)
+    }
+
+    function goToMatches() {
+        pushURL(`/game/matches`)
+    }
+
+    function goToEditPreferences() {
+        pushURL(`/users/${id}/edit/preferences`)
+    }
+
+    function goToUserProfile() {
+        pushURL(`/users/${id}`)
+    }
+
+    const handLink = <a className="rules link" onClick={()=>goToHand()}>hand</a>;
+
+    const playLink = <a className="rules link" onClick={()=>goToPlayWhites()}>play</a>;
+
+    const rateLink = <a className="rules link" onClick={()=>goToRateWhites()}>rate</a>;
+
+    const matchLink = <a className="rules link" onClick={()=>goToMatches()}>match</a>;
+
+    const editPreferencesLink = <a className="rules link" onClick={()=>goToEditPreferences()}>changing your preferences</a>;
+
+    const userProfileLink = <a className="rules link" onClick={()=>goToUserProfile()}>profile</a>;
+
+
+
+
 
 
     let information  = (
@@ -20,21 +73,21 @@ const Rules = () =>{
             <tr className="rules info-container">
                 <td>Every day you get to choose one Black Card which has a Gap which will be filled by other players by playing White Cards. <br/>
                     Other players can see you Black Card for 24 hours after you choose it.
-                    <br/>After 24 hours your "Hand" resets and you get to choose a new Black Card</td>
+                    <br/>After 24 hours your {handLink} resets and you get to choose a new Black Card</td>
 
 
             </tr>
             <div className="rules smallTitle"> White Cards </div>
             <tr className="rules info-container">
-                <td> Each day you get a fixed number of White Cards which you can play on other Users Black Cards. Try to use them in a way that is as funny or clever as possible.
-                    If the other user likes the white card you played you might match!
-                    <br/>After 24 hours your "Hand" resets and you get a new set of White Cards</td>
+                <td> Each day you get a fixed number of White Cards which you can {playLink} on other Users Black Cards. Try to use them in a way that is as funny or clever as possible.
+                    If the other user likes the white card you played you might {matchLink}!
+                    <br/>After 24 hours your {handLink} resets and you get a new set of White Cards</td>
 
             </tr>
             <div className="rules smallTitle"> Rating Cards & Matching </div>
             <tr className="rules info-container">
 
-                <td> Whenever someone has played a White Card on your Black Card you can Rate them by giving either a Thumbs up or Thumbs down. <br/>
+                <td> Whenever someone has played a White Card on your Black Card you can {rateLink} them by giving either a Thumbs up or Thumbs down. <br/>
                     When two players give each others White Cards a Thumbs Up they get "matched". Matched Users can chat with each other. <br/>
                     If you don't like the other user or are unsure you can either block them or unmatch them</td>
 
@@ -42,7 +95,7 @@ const Rules = () =>{
             <div className="rules smallTitle"> Preferences </div>
             <tr className="rules info-container">
 
-                <td> As not everyone is interested in everyone you can edit your preferences in your profile. <br/>
+                <td> As not everyone is interested in everyone you can edit your preferences in your {userProfileLink}. <br/>
                     You can select the Gender, Age and Radius that you are interested in so only Black Cards from users that fit these criteria get shown to you.
                     Similarly your Black Cards only get shown to users that are potentially interested in you.
                 </td>
@@ -51,7 +104,7 @@ const Rules = () =>{
             <div className="rules smallTitle"> No Black Cards to play on? </div>
             <tr className="rules info-container">
 
-                <td> If you still have White Cards left and there are no Black Cards left to play on consider changing your preferences so you might see more / different players Black Cards.
+                <td> If you still have White Cards left and there are no Black Cards left to play on consider {editPreferencesLink} so you might see more / different players Black Cards.
                 </td>
 
             </tr>
